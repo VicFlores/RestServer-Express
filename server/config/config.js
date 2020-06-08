83f7822d29ac11bdc9929e6ff9a9e99386e0d036
Vic Flores <vicsito2014@gmail.com>
2020-06-07 23:31:18 -0600

    Correcion de cadena de conexion por segunda vez

--- server/config/config.js
+++ server/config/config.js
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
 
-    urlDB = 'mongodb+srv://VicFlores11:vicsito11@cafe-sbof2.mongodb.net/<dbname>?retryWrites=true&w=majority'
+    urlDB = 'mongodb+srv://VicFlores11:vicsito11@cafe-sbof2.mongodb.net/cafe?retryWrites=true&w=majority'
 }
 
 process.env.URLDB = urlDB;
