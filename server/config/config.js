f98f843265122d0731935ddcfafc28e8901b3b9e
Vic Flores <vicsito2014@gmail.com>
2020-06-08 13:59:26 -0600

    Variable de entorno MongoDB

--- server/config/config.js
+++ server/config/config.js
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
 
-    urlDB = 'mongodb+srv://VicFlores11:vicsito11@cafe-sbof2.mongodb.net/cafe?retryWrites=true&w=majority'
+    urlDB = process.env.NODE_ENV.MongoDB_URI
 }
 
 process.env.URLDB = urlDB;
