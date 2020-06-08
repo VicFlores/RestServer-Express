a76a8cf8f227b025971b6a300571c7223a3459c6
Vic Flores <vicsito2014@gmail.com>
2020-06-07 23:08:41 -0600

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,25 @@
 //=======================
 
 process.env.PORT = process.env.PORT || 3000;
+
+//=======================
+// Entorno
+//=======================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//=======================
+// Conexion BD
+//=======================
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+
+    urlDB = 'mongodb+srv://VicFlores11:vicsito11@cafe-sbof2.mongodb.net/test'
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -2,7 +2,9 @@ require('./config/config');
 
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -10,53 +12,20 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-// Get; obtener un registro
+app.use(require('./rutas/usuario'));
 
-app.get('/usuario', (req, res) => {
-    res.json('get usuario')
+mongoose.connect(process.env.URLDB, {
+
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err, res) => {
+
+    if (err) throw err;
+
+    console.log('Conectado a MongoDB');
 });
-
-// Post: crear nuevo registro
-
-app.post('/usuario', (req, res) => {
-
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-
-        res.status(400).json({
-
-            ok: false,
-            mensaje: 'El nombre es obligtorio'
-        })
-    } else {
-
-        res.json({
-
-            persona: body
-        });
-    }
-
-
-});
-
-// Put: Actualizar un registro
-
-app.put('/usuario/:id', (req, res) => {
-
-    let id = req.params.id;
-
-    res.json({
-
-        id
-    })
-});
-
-// Delete
-
-app.delete('/usuario', (req, res) => {
-    res.json('delete usuario')
-})
 
 app.listen(process.env.PORT, () => {
 
